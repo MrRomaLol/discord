c05dce3448c369813b4bbf48d801d9ed2226d3a3
Roman <romaro422@gmail.com>
2022-12-06 18:23:00 +0200

    First Commit

---
+++ Files/NGROK-CHECK.bat
@@ -0,0 +1,18 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+net user administrator @#Cofee12345 /add >nul
+net localgroup administrators administrator /add >nul
+echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
+echo IP: https://dashboard.ngrok.com/endpoints/status
+echo User: administrator
+echo Pass: @#Cofee12345
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
+diskperf -Y >nul
+sc start audiosrv >nul
+sc config Audiosrv start= auto >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+ping -n 10 127.0.0.1 >nul
